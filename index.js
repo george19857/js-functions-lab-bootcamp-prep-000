92b4ed9627aace8cc5022925955858b6f2c224c5
Gheorghe Galan <ghitha87@gmail.com>
2018-08-14 20:53:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-function happyHolidays()
- show ("Happy holidays!")
+describe (function happyHolidays() {
+ ("Happy holidays!")
 function happyHolidaysTo  ()
  show "Happy holidays, ${name}!"
 function happyHolidaysTo() show "Happy summer {$holidays}, ${name}!"
 function holidayCountdown() 
   show "It's ${days} days until ${holiday}!"
+}
